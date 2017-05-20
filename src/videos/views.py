8c6df4a8240d5bae73e5f6d9836cd287c56038f7
Belonias <iakovosbelonias@gmail.com>
2017-05-20 23:10:45 +0300

    cbv skeleton

--- src/videos/views.py
+++ src/videos/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,
+    UpdateView,
+    DeleteView,
+    )
+from .models import Video
 
 # Create your views here.
+class VideoCreateView(CreateView):
+    queryset = Video.objects.all()
+    pass
+
+
+class VideoDetailView(DetailView):
+    queryset = Video.objects.all()
+    pass
+
+
+class VideoListView(ListView):
+    queryset = Video.objects.all()
+    pass
+
+
+class VideoUpdateView(UpdateView):
+    queryset = Video.objects.all()
+    pass
+
+
+class VideoDeleteView(DeleteView):
+    queryset = Video.objects.all()
+    pass
